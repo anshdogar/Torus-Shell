b1d11932f9f69576c58a76117bc1e7ce84f71cea
Ansh Dogar <a.dogar@student.tudelft.nl>
2026-06-07 22:23:06 +0200

    get command line commands (except cd)

--- shell.c
+++ shell.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 #define MAX_INPUT 1024
+#define MAX_ARGS 64
 int main(){
+
     char input[MAX_INPUT];
+
+    char* args[MAX_ARGS];
 
     while (1)
     {
@@ -13,21 +20,46 @@ int main(){
 
         fflush(stdout);
 
-        if(fgets(input, MAX_INPUT, stdin) == NULL){
-            perror("Torus failed, please try again");
+        if(fgets(input, MAX_INPUT, stdin) == NULL){ //preventive guard
+            perror("Torus failed, please try again.\n");
             continue;
         }
 
-        input[strcspn(input, "\n")] = '\0';
+        input[strcspn(input, "\n")] = '\0'; //takes input string and changes all instances of new line to end character
 
-        if(strcmp(input, "exit") == 0){
+        if(strcmp(input, "exit") == 0){ //compares strings for values
             printf("Goodbye.\n");
             break;
         }
-    printf("%s\n", input);
+    char* token = strtok(input, " ");
+    int i = 0;
+    while (token != NULL && i < MAX_ARGS)
+    {
+        args[i] = token;
+        token = strtok(NULL, " ");
+        i++;
+    }
 
+    args[i] = NULL;
+
+    pid_t pid = fork();
+
+    if(pid == 0){
+        execvp(args[0], args);
+        perror("Torus failed, please try again.\n");
+        exit(EXIT_FAILURE);
+    }
+    else if(pid > 0){
+        int status;
+        waitpid(pid, &status, 0);
+        printf("Existing status: %d\n", status);
+    }
+
+    else{
+       perror("Torus failed, please try again.\n"); 
     }
     
+}
 
     return 0;
 }
